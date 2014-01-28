a8806c8c99d5846204f6c6a0ce46255f4bd6d3b9
Eugene <yeo.eugene.oey@gmail.com>
2014-01-28 13:30:29 +0800

    Parameterized Testing for OverlapRectangleTest

--- lab/src/lab1/ex1/OverlapRectanglesTest.java
+++ lab/src/lab1/ex1/OverlapRectanglesTest.java
@@ -3,62 +3,56 @@ package lab1.ex1;
 import static org.junit.Assert.*;
 
 import java.awt.Point;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(value=Parameterized.class)
 public class OverlapRectanglesTest {
 
-    @Before
-    public void setUp() throws Exception {
+    @Parameters
+    public static Collection<Object[]> getTestParameters(){
+        return Arrays.asList(new Object[][]{
+                // expected, rectangle1, rectangle2
+                {24, "3 -1 15 6", "19 2 9 10"},
+                {24, "15 6 3 -1", "9 2 19 10"},
+                {0, "-5 5 -1 1", "1 2 11 12"},
+                {10, "-2 -5 8 0", "-10 -9 0 4"},
+                {200, "70 60 50 0", "20 20 100 10"}
+        });
     }
-
+    
+    private int expected;
+    private Point r1p1;
+    private Point r1p2;
+    private Point r2p1;
+    private Point r2p2;
+    
+    public OverlapRectanglesTest(int expected, String rect1, String rect2){
+        this.expected = expected;
+        String[] rect1_raw_input = rect1.split("\\s+");
+        String[] rect2_raw_input = rect2.split("\\s+");
+        
+        this.r1p1 = new Point((int)Integer.parseInt(rect1_raw_input[0]), 
+                Integer.parseInt(rect1_raw_input[1]));
+        this.r1p2 = new Point((int)Integer.parseInt(rect1_raw_input[2]), 
+                Integer.parseInt(rect1_raw_input[3]));
+        this.r2p1 = new Point((int)Integer.parseInt(rect2_raw_input[0]), 
+                Integer.parseInt(rect2_raw_input[1]));
+        this.r2p2 = new Point((int)Integer.parseInt(rect2_raw_input[2]), 
+                Integer.parseInt(rect2_raw_input[3]));
+    }
+    
     @Test
-    public void testOverlapArea() {
-        // Input 1:
-        // 3 -1 15 6
-        // 19 2 9 10
-        int area1 = (int) OverlapRectangles.overlapArea(
-                new Point(3, -1), new Point(15, 6),
-                new Point(19, 2), new Point(9, 10));
-        // Output 1: 24
-        assertEquals(24, area1);
-        
-        // Input 2:
-        // 15 6 3 -1
-        // 9 2 19 10
-        int area2 = (int) OverlapRectangles.overlapArea(
-                new Point(15, 6), new Point(3, -1),
-                new Point(9, 2), new Point(19, 10));
-        // Out: 24
-        assertEquals(24, area2);
-        // Input 3:
-        // -5 5 -1 1
-        // 1 2 11 12
-        int area3 = (int) OverlapRectangles.overlapArea(
-                new Point(-5, 5), new Point(-1, 1),
-                new Point(1, 2), new Point(11, 12));
-        // output 3: 0
-        assertEquals(0, area3);
-        
-        
-        // input 4:
-        // -2 -5 8 0
-        // -10 -9 0 4
-        int area4 = (int) OverlapRectangles.overlapArea(
-                new Point(-2, -5), new Point(8, 0),
-                new Point(-10, -9), new Point(0, 4));
-        // output 4: 10
-        assertEquals(10, area4);
-        
-        // input 5:
-        // 70 60 50 0
-        // 20 20 100 10
+    public void testOverlapArea(){
         int area = (int) OverlapRectangles.overlapArea(
-                new Point(70, 60), new Point(50, 0),
-                new Point(20, 20), new Point(100, 10));
-        // output 5: 200
-        assertEquals(200, area);
+                this.r1p1, this.r1p2, this.r2p1, this.r2p2);
+        assertEquals(this.expected, area);
     }
-
 }
