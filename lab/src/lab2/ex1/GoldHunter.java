ac883c926c788a377ed463ce9597597c229c12fc
Eugene <yeo.eugene.oey@gmail.com>
2014-02-11 02:22:31 +0800

    Document class, add particular info, and reformat code

--- lab/src/lab2/ex1/GoldHunter.java
+++ lab/src/lab2/ex1/GoldHunter.java
@@ -2,13 +2,13 @@ package lab2.ex1;
 
 // CS1020 (AY2013/4 Semester 2)
 // Take-home lab #2
-// Name: 
-// Matric. No.: 
-// Lab group: 
-// Write the program description below.
-// It is mandatory to write program description at the top of every program.
-// Marks will be awarded for this in sit-in labs.
-// Please remove this line and its preceding 3 lines.
+// Name: Eugene
+// Matric. No.: A0116631N
+// Lab group: 7
+/**
+ * This apps will provide a Gold Value mapping for each cell.
+ * An increase of Gold value when the bomb is nearby.
+ */
 
 import java.util.*;
 
@@ -16,19 +16,18 @@ public class GoldHunter {
 
     private static final int GOLD_VALUE_INCREMENT = 1;
     private static final char BOMB_LABEL = '*';
-    
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         MyMap map = new MyMap(sc);
-        
+
         // Avoid static class and create an instance of our program instead.
         GoldHunter gh = new GoldHunter();
-        
+
         // Generate the GOLD VALUE
         map = gh.generateGoldMap(map);
-        
+
         // Build the String to be displayed
         String gold_map = gh.buildGoldMapString(map);
         System.out.print(gold_map);
@@ -37,18 +36,18 @@ public class GoldHunter {
     /**
      * Helper function as a String Builder, to prepare for the presentation.
      * 
-     * @param map 
+     * @param map
      * @return String
      */
     private String buildGoldMapString(MyMap map) {
         String result = "";
-        
-        for (int row = 0; row < map.getNumRow(); row++){
-            
-            for (int col = 0; col < map.getNumCol(); col++){
-                
-                if (this.isBomb(row, col, map)){
-                    result += BOMB_LABEL; 
+
+        for (int row = 0; row < map.getNumRow(); row++) {
+
+            for (int col = 0; col < map.getNumCol(); col++) {
+
+                if (this.isBomb(row, col, map)) {
+                    result += BOMB_LABEL;
                 } else {
                     int gold_value = map.getGoldVal(row, col);
                     result += gold_value;
@@ -63,8 +62,8 @@ public class GoldHunter {
     /**
      * Generate the entire map with the gold values
      * 
-     * Gold values is depends on the neighboring bombs. 
-     * The more bombs nearby, the more gold it has.
+     * Gold values is depends on the neighboring bombs. The more bombs nearby,
+     * the more gold it has.
      * 
      * @param map
      * @return map
@@ -72,21 +71,21 @@ public class GoldHunter {
     private MyMap generateGoldMap(MyMap map) {
         int max_col = map.getNumCol();
         int max_row = map.getNumRow();
-        
+
         // Iterate each row and column
-        for (int row = 0; row < max_row; row++){
-            for (int col = 0; col < max_col; col++){
-                
+        for (int row = 0; row < max_row; row++) {
+            for (int col = 0; col < max_col; col++) {
+
                 // Check whether the current row, col is a bomb
-                if (this.isBomb(row, col, map)){
-                    
+                if (this.isBomb(row, col, map)) {
+
                     // if location row, col is a bomb
                     // increase its neighbor gold value
                     map = this.increaseNeighborGoldValue(row, col, map);
                 }
             }
         }
-        
+
         return map;
     }
 
@@ -94,14 +93,13 @@ public class GoldHunter {
      * Increase the Gold Value of the neighbor
      * 
      * As long as its neighbor is not a bomb, the gold value will increase.
-     * Please note however, this function does not validate 
-     *    whether the provided row, col in the parameter is a bomb.
-     *    Therefore, if the program need to have this restriction, 
-     *    do the validation on a separate method before calling this method
-     *    
-     * as long as you call this method is called
-     *    the neighbour gold value increase 
-     *    by the GOLD_VALUE_INCREMENT constant
+     * Please note however, this function does not validate whether the provided
+     * row, col in the parameter is a bomb. Therefore, if the program need to
+     * have this restriction, do the validation on a separate method before
+     * calling this method
+     * 
+     * as long as you call this method is called the neighbour gold value
+     * increase by the GOLD_VALUE_INCREMENT constant
      * 
      * @param row
      * @param col
@@ -111,20 +109,20 @@ public class GoldHunter {
     private MyMap increaseNeighborGoldValue(int row, int col, MyMap map) {
         // Top neighbor with restriction of the top boundary
         int top = Math.max(0, row - 1);
-        // Bottom neighbor with restriction of the bottom boundary        
-        int bottom = Math.min(row + 1, map.getNumRow() - 1); 
-        // Left neighbor with restriction of the left boundary        
+        // Bottom neighbor with restriction of the bottom boundary
+        int bottom = Math.min(row + 1, map.getNumRow() - 1);
+        // Left neighbor with restriction of the left boundary
         int left = Math.max(0, col - 1);
         // Right neighbor with restriction of the right boundary
         int right = Math.min(col + 1, map.getNumCol() - 1);
-        
+
         // Iterate Neighbors (r: iterating row, c: iterating column)
-        for (int r = top; r <= bottom; r++){
-            for (int c = left; c <= right; c++){
+        for (int r = top; r <= bottom; r++) {
+            for (int c = left; c <= right; c++) {
                 // Exclude non-neighbor
-                if (r != row || c != col){
+                if (r != row || c != col) {
                     // if NOT a bomb
-                    if (!this.isBomb(r, c, map)){
+                    if (!this.isBomb(r, c, map)) {
                         // increase the Gold Value
                         int gold_value = map.getGoldVal(r, c);
                         gold_value += GOLD_VALUE_INCREMENT;
@@ -137,16 +135,15 @@ public class GoldHunter {
     }
 
     /**
-     * We don't have to worry about the bomb character anymore,
-     * This method abstract the bomb character.
+     * We don't have to worry about the bomb character anymore, This method
+     * abstract the bomb character.
      * 
      * @param character
      * @return boolean
      */
-    private boolean isBomb(int row, int col, MyMap map){
-        
+    private boolean isBomb(int row, int col, MyMap map) {
+
         // Check if the current row, col label is a BOMB_LABEL
         return map.getLabel(row, col) == BOMB_LABEL;
     }
 }
-
--- lab/src/lab2/ex1/MyMap.java
+++ lab/src/lab2/ex1/MyMap.java
@@ -2,9 +2,9 @@ package lab2.ex1;
 
 // CS1020 (AY2013/4 Semester 2)
 // Take-home lab #2
-// Name: 
-// Matric. No.: 
-// Lab group: 
+// Name: Eugene
+// Matric. No.: A0116631N
+// Lab group: 7
 // The MyMap class consists of 2 two-dimensional arrays,
 // the character grid representing the map, and an integer array
 // that stores the number of neighbouring mines in each mine-free location.
@@ -13,78 +13,79 @@ import java.util.*;
 
 class MyMap {
     private char[][] charMap; // Map containing the characters (labels)
-    private int[][] intMap;  // Map containing the gold values
+    private int[][] intMap; // Map containing the gold values
     private int nRow; // number of rows
     private int nCol; // number of columns
 
-    public MyMap() {};  
+    public MyMap() {
+    };
 
-    public MyMap(Scanner sc) {       
+    public MyMap(Scanner sc) {
         nRow = sc.nextInt();
         nCol = sc.nextInt();
         charMap = new char[nRow][nCol];
-        intMap = new int[nRow][nCol];   
+        intMap = new int[nRow][nCol];
         String line;
         for (int i = 0; i < nRow; i++) {
             line = sc.next();
-            for (int j = 0; j < nCol; j++) {        
+            for (int j = 0; j < nCol; j++) {
                 setLabel(i, j, line.charAt(j));
-                setGoldVal(i, j, 0);        
+                setGoldVal(i, j, 0);
             }
         }
     }
 
-    public int getNumRow() { 
-        return nRow; 
+    public int getNumRow() {
+        return nRow;
     }
 
-    public int getNumCol() { 
-        return nCol; 
+    public int getNumCol() {
+        return nCol;
     }
 
-    public char getLabel(int row, int col) { 
-        return charMap[row][col]; 
+    public char getLabel(int row, int col) {
+        return charMap[row][col];
     }
 
-    public int getGoldVal(int row, int col) { 
-        return intMap[row][col]; 
+    public int getGoldVal(int row, int col) {
+        return intMap[row][col];
     }
 
-    public void setLabel(int row, int col, char label) { 
-        charMap[row][col] = label; 
+    public void setLabel(int row, int col, char label) {
+        charMap[row][col] = label;
     }
 
-    public void setGoldVal(int row, int col, int val) { 
-        intMap[row][col] = val; 
+    public void setGoldVal(int row, int col, int val) {
+        intMap[row][col] = val;
     }
 
     // To print the character map
     public void display() {
         String result = "";
-        
-        for (int row = 0; row < this.getNumRow(); row++){
-            
+
+        for (int row = 0; row < this.getNumRow(); row++) {
+
             char[] cols = this.charMap[row];
-            
-            for (int col = 0; col < this.getNumCol(); col++){
+
+            for (int col = 0; col < this.getNumCol(); col++) {
                 char mine_count = cols[col];
                 // Not a good Practice (Less Readability), forgive me.
                 // to improve, please use "if-else" statement
                 result += (mine_count == -1) ? "*" : mine_count;
             }
-            
-            // If it is the last row don't add a new line, 
+
+            // If it is the last row don't add a new line,
             // because it break the test case.
-            if (row < this.getNumRow() - 1){
+            if (row < this.getNumRow() - 1) {
                 result += "\n";
             }
         }
-        
-        // This is not a good practice. (It make testing harder, 
+
+        // This is not a good practice. (It make testing harder,
         // due to side effect to the sysout)
-        // To make this method much cleaner and testable, 
+        // To make this method much cleaner and testable,
         // it should return the string instead of printing it.
-        // thus the methods should be called generateCharacterMap 
+        // thus the methods should be called generateCharacterMap
         // and return the string
         // return result;
         System.out.print(result);
@@ -92,19 +93,18 @@ class MyMap {
 
     // Return true if row, col are within valid range,
     // otherwise return false.
+    // Unfortunately, I don't know what in scenario to use this methods.
     public boolean indexWithinRange(int row, int col) {
         // Assume this.intMap is not empty array (or 0 length).
         int max_row = this.intMap.length;
-        
+
         // Assuming that it is not a dynamic array size
         int max_col = this.intMap[0].length;
-        
-        // if row smaller than maximum row size 
-        //      and col is smaller than maximum column size, 
-        //      then it should be true otherwise false
-        return row < max_row && col < max_col;
 
+        // if row smaller than maximum row size
+        // and col is smaller than maximum column size,
+        // then it should be true otherwise false
+        return row < max_row && col < max_col;
 
     }
 }
-
