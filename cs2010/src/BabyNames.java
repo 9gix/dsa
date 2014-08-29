24243b0567b081e278379807e127273a3e62284e
Eugene <yeo.eugene.oey@gmail.com>
2014-08-29 22:10:48 +0800

    BabyName Tree implementation
    recursive query for counting the matching names
    iteratively inserting name

--- cs2010/src/BabyNames.java
+++ cs2010/src/BabyNames.java
@@ -1,9 +1,15 @@
 import java.util.*;
 import java.io.*;
 
-// write your matric number here: A0116631N
-// write your name here: Eugene
-// write list of collaborators here (reading someone's post in Facebook group and using the idea is counted as collaborating):
+// Matric number: A0116631N
+// Name: Eugene
+
+// list of collaborators:
+// - Introduction to Algorithms: 
+//     Thomas H. Cormen, 
+//     Charles E. Leiserson,
+//     Ronald L. Rivest,
+//     Clifford Stein.
 
 enum Gender {
 	MALE,
@@ -13,10 +19,12 @@ enum Gender {
 /**
  * Baby Name Data Structure
  */
-class BabyName implements Comparable<String>{
+class BabyName implements Comparable<BabyName>{
 
 	private String _name;
 	private Gender _gender;
+	private BabyName _left;
+	private BabyName _right;
 	
 	public BabyName(String name, Gender gender){
 		this._name = name;
@@ -32,17 +40,119 @@ class BabyName implements Comparable<String>{
 	}
 
 	@Override
-  public int compareTo(String name) {
-		return this.getName().compareTo(name);
+  public int compareTo(BabyName bn) {
+		return this.getName().compareTo(bn.getName());
   }
+
+	public BabyName getRight() {
+	  return _right;
+  }
+
+	public void setRight(BabyName _right) {
+	  this._right = _right;
+  }
+
+	public BabyName getLeft() {
+	  return _left;
+  }
+
+	public void setLeft(BabyName _left) {
+	  this._left = _left;
+  }
+
+	public void setParent(BabyName _parent) {
+		if (_parent != null){
+			if (_parent.compareTo(this) < 0){
+				_parent.setLeft(this);
+			} else {
+				_parent.setRight(this);
+			}
+		}
+  }
+}
+
+class MaleBabyName extends BabyName {
+
+	public MaleBabyName(String name) {
+	  super(name, Gender.MALE);
+  }
+	
+}
+
+class FemaleBabyName extends BabyName {
+	public FemaleBabyName(String name){
+		super(name, Gender.FEMALE);
+	}
+}
+interface ITree {
+	
+}
+
+class BabyTree<T> implements ITree {
+	
+	private BabyName _root; 
+	
+	public BabyTree(Comparator<BabyName> babyname_comparator) {
+		
+  }
+
+	int count_match(String start, String end){
+		return recursive_name_count(this._root, start, end);
+	}
+	
+	int recursive_name_count(BabyName babyname, String start, String end){
+		if (babyname == null){
+			return 0;
+		} else {
+
+			int lexical_start_bound = babyname.getName().compareTo(start);
+			int lexical_end_bound = babyname.getName().compareTo(end);
+			
+			if (lexical_start_bound >= 0 && lexical_end_bound < 0){
+				int left = recursive_name_count(babyname.getLeft(), start, end);
+				int right = recursive_name_count(babyname.getRight(), start, end);
+				return left + 1 + right;
+			} else if (lexical_start_bound < 0){
+				int right = recursive_name_count(babyname.getRight(), start, end);
+				return right;
+			} else if (lexical_end_bound >= 0){
+				int left = recursive_name_count(babyname.getLeft(), start, end);
+				return left;
+			} else {
+				return 0;
+			}
+		}
+	}
+
+	public void insert(BabyName bn) {
+  	BabyName _parent = null;
+  	BabyName _current = this._root;
+  	while (_current != null){
+  		_parent = _current;
+  		if (bn.compareTo(_current) < 0){
+  			_current = _current.getLeft();
+  		} else {
+  			_current = _current.getRight();
+  		}
+  	}
+
+	  if (_parent == null){
+	  	this._root = bn;
+	  } else if (bn.compareTo(_parent) < 0) {
+	  	_parent.setLeft(bn);
+	  } else {
+	  	_parent.setRight(bn);
+	  }
+	}
+	
 }
 
 class BabyNames {
 
   // Declaration
   // --------------------------------------------
-	private TreeSet<BabyName> _female_baby_names;
-	private TreeSet<BabyName> _male_baby_names;
+	private BabyTree<FemaleBabyName> _female_baby_names;
+	private BabyTree<MaleBabyName> _male_baby_names;
   // --------------------------------------------
 
   public BabyNames() {
@@ -50,12 +160,12 @@ class BabyNames {
   		
 			@Override
       public int compare(BabyName o1, BabyName o2) {
-				return o1.compareTo(o2.getName());		  
+				return o1.compareTo(o2);		  
       }
 			
 		};
-  	_female_baby_names = new TreeSet<BabyName>(babyname_comparator);
-  	_male_baby_names = new TreeSet<BabyName>(babyname_comparator);
+  	_female_baby_names = new BabyTree<FemaleBabyName>(babyname_comparator);
+  	_male_baby_names = new BabyTree<MaleBabyName>(babyname_comparator);
   }
   
   private Gender getGender(int genderId){
@@ -79,9 +189,9 @@ class BabyNames {
   	BabyName bn = new BabyName(babyName, _gender);
   	
   	if (_gender == Gender.MALE){
-  		this._male_baby_names.add(bn);
+  		this._male_baby_names.insert(bn);
   	} else if (_gender == Gender.FEMALE){
-  		this._female_baby_names.add(bn);
+  		this._female_baby_names.insert(bn);
   	}
     // --------------------------------------------
   }
@@ -94,26 +204,13 @@ class BabyNames {
     // --------------------------------------------
     // System.out.println(START + ", " + END);
     Gender gender_pref = getGender(genderPreference);
+    
     if (gender_pref == Gender.MALE || gender_pref == null) {
-    	for (BabyName baby_name: this._male_baby_names){		
-  			int lexical_start_bound = baby_name.compareTo(START);
-  			int lexical_end_bound = baby_name.compareTo(END);
-  			if (lexical_start_bound >= 0 && lexical_end_bound < 0){
-  				ans += 1;
-  			}
-//  			System.out.println(baby_name.getName() + ":" + lexical_start_bound + ".." +lexical_end_bound);
-    	}
+    	ans += this._male_baby_names.count_match(START, END);
     }
     
     if (gender_pref == Gender.FEMALE || gender_pref == null){
-    	for (BabyName baby_name: this._female_baby_names){		
-  			int lexical_start_bound = baby_name.compareTo(START);
-  			int lexical_end_bound = baby_name.compareTo(END);
-  			if (lexical_start_bound >= 0 && lexical_end_bound < 0){
-  				ans += 1;
-  			}
-//  			System.out.println(baby_name.getName() + ":" + lexical_start_bound + ".." +lexical_end_bound);
-    	}
+    	ans += this._female_baby_names.count_match(START, END);
     }
     // --------------------------------------------
 
